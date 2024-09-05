037d8c18935fae149647cdb82883fc38f8ae28ea
mimaverde <verdemima@gmail.com>
2024-09-05 20:08:36 -0300

    bubbles

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// const pastelColors = [
-//     '#FFFFFF', '#cc5ef5', '#461791'
-// ];
+const pastelColors = [
+    '#FFFFFF', '#cc5ef5', '#461791'
+];
   
-// createCircles = () => {
-//     const section = document.getElementById('bubbles');
-//     const circles = document.createElement('span');
+createCircles = () => {
+    const section = document.getElementById('bubbles');
+    const circles = document.createElement('span');
 
-//     const size = Math.random() * 25;
+    const size = Math.random() * 25;
 
-//     circles.style.width = 10 + size + 'px';
-//     circles.style.height = 10 + size + 'px';
+    circles.style.width = 10 + size + 'px';
+    circles.style.height = 10 + size + 'px';
 
-//     circles.style.top = Math.random() * innerHeight + 'px';
-//     circles.style.left = Math.random() * innerWidth + 'px';
+    circles.style.top = Math.random() * innerHeight + 'px';
+    circles.style.left = Math.random() * innerWidth + 'px';
 
-//     circles.style.background = pastelColors[Math.floor(Math.random() * pastelColors.length)];
-//     section.appendChild(circles);
+    circles.style.background = pastelColors[Math.floor(Math.random() * pastelColors.length)];
+    section.appendChild(circles);
 
-//     setTimeout(() => {
-//         circles.remove()
-//     }, 5000);
-// }
+    setTimeout(() => {
+        circles.remove()
+    }, 5000);
+}
 
-// setInterval(createCircles, 150);
+setInterval(createCircles, 150);
 
 document.addEventListener('DOMContentLoaded', () => {
     const seeMoreElement = document.getElementById('seeMore');
@@ -42,33 +42,5 @@ document.addEventListener('DOMContentLoaded', () => {
             elementToShow.classList.add('page--hidden');
         });
     }
-
-    document.querySelector(".menu-responsive").style.display = "none";
-
-    document.querySelector(".menu-bars").addEventListener("click", function() {
-        const menu = document.querySelector(".menu-responsive");
-        if (menu.style.display === "none" || menu.style.display === "") {
-            menu.style.display = "block";
-            menu.style.transition = "display 0.5s ease-in-out";
-        } else {
-            menu.style.display = "none";
-        }
-    });
-
-    document.querySelectorAll(".scroll").forEach(anchor => {
-        anchor.addEventListener("click", function(e) {
-            e.preventDefault();
-    
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-    
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: "smooth"
-                });
-            }
-        });
-    });
 });
 
