b63399de9dd023db161aac31563a396a6aac00f0
Mima Verde <verdemima@gmail.com>
2024-09-05 21:56:35 -0300

    New random

--- script.js
+++ script.js
@@ -35,19 +35,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const seeMoreElement = document.getElementById('seeMore');
     const elementToShow = document.getElementById('pageToShow');
     const showHome = document.getElementById('seeHome');
+    const closeAll = document.getElementById('isHome');
+    const aboutUs = document.getElementById('aboutUs');
 
     if (elementToShow) {
         seeMoreElement.addEventListener('click', (e) => {
             e.preventDefault();
             elementToShow.classList.add('page--current');
             elementToShow.classList.remove('page--hidden');
+            closeAll.classList.remove('selected');
+            aboutUs.classList.add('selected');
         });
 
         showHome.addEventListener('click', (e) => {
             e.preventDefault();
             elementToShow.classList.remove('page--current');
             elementToShow.classList.add('page--hidden');
+            closeAll.classList.add('selected');
+            aboutUs.classList.remove('selected');
         });
+
+        closeAll.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(elementToShow.classList.contains('page--current')){
+                elementToShow.classList.remove('page--current');
+                elementToShow.classList.add('page--hidden');
+                closeAll.classList.add('selected');
+                aboutUs.classList.remove('selected');
+            }
+        });
+
+        aboutUs.addEventListener('click', (e) => {
+            e.preventDefault();
+            aboutUs.classList.add('selected');
+            closeAll.classList.remove('selected');
+            elementToShow.classList.add('page--current');
+            elementToShow.classList.remove('page--hidden');
+        })
     }
 });
 
